793a67618f141bb89119e489a15e9e6c7ce3dfc8
אמיר ליכטר <lichtea3@macs.biu.ac.il>
2018-05-24 14:43:02 +0300

    with comments.

--- Ex2.py
+++ Ex2.py
@@ -1,6 +1,8 @@
 import string
 from random import shuffle
 
+
+# the main class.
 class GeneticAlgorithm:
     def __init__(self):
         self.dictionary = ""
@@ -10,14 +12,17 @@ class GeneticAlgorithm:
         self.reverse_permutation = {}
         self.score = {}
 
+    # read the encrypt file to string var
     def read_enc_file(self):
         with open("enc1.txt", 'r') as enc_file:
             self.enc = enc_file.read()
 
+    # read the dict file to string var
     def read_dict_file(self):
         with open("dict.txt", 'r') as dict_file:
             self.dictionary = dict_file.read()
 
+    # init the permutation var
     def initialize_permutation(self):
         letters = list(string.ascii_lowercase)
         initial_enc = list(letters)
@@ -27,12 +32,14 @@ class GeneticAlgorithm:
         self.permutation = initial_permutation
         print(self.permutation)
 
+    # init the score of every letter in the current permutation.
     def initialize_score(self):
         letters = list(string.ascii_lowercase)
         for letter in letters:
             self.score[letter] = 0
         print(self.score)
 
+    # evaluation function for every letter in the current permutation
     def evaluation(self):
         word = ""
         copy_text = self.enc
@@ -51,6 +58,7 @@ class GeneticAlgorithm:
                 word = ""
         print(self.score)
 
+    # printer function
     def printer(self):
         print(self.enc)
         print("-------------------------------------------------------------------------------------------------------")
